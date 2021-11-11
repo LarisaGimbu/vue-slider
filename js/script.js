0b5e4bcdee4e0975eeec4f269dc4ca864d64d367
LarisaGimbu <gimbularisa@gmail.com>
2021-11-11 14:40:49 +0100

    array items

--- js/script.js
+++ js/script.js
@@ -1,6 +1,38 @@
 const app = new Vue({
   el:'#app',
   data:{
-    
+    items:[
+      {
+        name: 'Svezia',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+        image:'img/01.jpg',
+      },
+      {
+        name: 'Svizzera',
+        text: 'Lorem ipsu',
+        image:'img/02.jpg',
+      },
+      {
+        name: 'Gran Bretagna',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        image:'img/03.jpg',
+      },
+      {
+        name: 'Gran Bretagna',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        image:'img/04.jpg',
+      },
+      {
+        name: 'Germania',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+        image:'img/05.jpg',
+      },
+      {
+        name: 'Paradise',
+        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+        image:'img/06.jpg',
+      },
+      
+    ]
   }
 });
