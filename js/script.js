b4d9dcbded9e099e792ce05de9cab009447fe4c7
LarisaGimbu <gimbularisa@gmail.com>
2021-11-11 17:14:25 +0100

    carousel center image

--- js/script.js
+++ js/script.js
@@ -6,28 +6,53 @@ const app = new Vue({
         name: 'Svezia',
         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
         image:'img/01.jpg',
+        miaClasse:'active',
       },
       {
         name: 'Svizzera',
         text: 'Lorem ipsu',
         image:'img/02.jpg',
+        miaClasse:'',
       },
       {
         name: 'Gran Bretagna',
         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
         image:'img/03.jpg',
+        miaClasse:'',
       },
       {
         name: 'Germania',
         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
         image:'img/04.jpg',
+        miaClasse:'',
       },
       {
         name: 'Paradise',
         text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
         image:'img/05.jpg',
+        miaClasse:'',
       },
       
-    ]
+    ],
+    counter: 0,
+  },
+  methods:{
+    nextSlide(){
+      this.counter++;
+      console.log(this.counter);
+      if(this.counter > this.items.length -1){
+        this.counter = 0
+      };
+      this.items[this.counter].miaClasse = 'active';
+      this.items[this.counter-1].miaClasse = '';
+    },
+    prevSlide(){
+      this.counter--;
+      if(this.counter > this.items.length){
+        counter=0
+      };
+      this.items[counter].miaClasse = 'active';
+      this.items[counter-1].miaClasse ='';
+    }
   }
 });
